16fb8000ee84fa9e92d7cb2ead8111f4c7982e9c
Burbulis <onehalf3554@mail.ru>
2020-10-06 16:55:21 +0300

    Add files via upload

--- cell.c
+++ cell.c
@@ -1,4 +1,4 @@
-coordinates to_cell(char *cell_str)
+/*coordinates to_cell(char *cell_str)
 {
 	char cell_name[]="ABCDE";
 	char cell_index[]="01234";
@@ -28,6 +28,44 @@ coordinates to_cell(char *cell_str)
 	}
 
 
+	coordinates cell;
+	if ((!!x_name)&&(!!x_index))
+	{
+		cell.name  = cell_name[i_name];
+		cell.index = cell_idx[i_idx];
+	}
+	return (cell);
+}
+*/
+coordinates to_cell(char *cell_str)
+{
+	char cell_name[]="ABCDE";
+	char cell_index[]="01234";
+	unsigned int cell_idx[] = {0,1,2,3,4};
+
+	char *x_name = NULL;
+	char *x_index = NULL;
+	unsigned int i_name , i_idx;
+	for (unsigned int i = 0 ; i < strlen(cell_name);++i)
+	{
+		i_name = i;
+		x_name = strchr(cell_str,cell_name[i]);
+		x_index = strchr(cell_str,cell_index[i]);
+		if (!!x_index)
+		{
+			i_idx = i;
+		}
+		if (!!x_name)
+		{
+			i_name = i;
+		}
+
+		if ((!!x_index)&&(!!x_name))  
+		{
+			break;
+	  	}
+	}
+    
 	coordinates cell;
 	if ((!!x_name)&&(!!x_index))
 	{
