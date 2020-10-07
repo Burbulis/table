641b75fd6b4aba09d6edb3c69401f6fe8bc29409
Burbulis <onehalf3554@mail.ru>
2020-10-07 15:29:00 +0300

    Add files via upload

--- cell.h
+++ cell.h
@@ -1,6 +1,28 @@
 #ifndef _CELL_
 #define _CELL_
-#include "hash.h"
- coordinates to_cell(char *cell_str);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CELL( cell_ ) to_cell( cell_ )
+#define SET_CELL(cell) cell_normal(cell)
+
+struct coordinates
+{
+  char name;
+  unsigned long index;
+};
+
+struct _cell
+{
+  coordinates c;
+  unsigned int value;
+};
+
+
+coordinates to_cell(char *cell_str);
+_cell cell_normal(char *str);
+
 #endif
 
